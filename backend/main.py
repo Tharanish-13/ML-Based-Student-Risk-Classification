a9b3db8a1e918a05047dff9da50ea9fce0d2d3e4
THARANISH J <157452382+Tharanish-13@users.noreply.github.com>
2026-04-06 15:22:25 +0530

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
-    "https://ml-based-student-risk-classificatio.vercel.app"
+    "https://ml-based-student-risk-classificatio.vercel.app",
+    "https://mlbasedstudentriskclassificationsystem-6s03vg18w.vercel.app"
 ]
 
 app.add_middleware(
